e844944c320aaf020c4e23d4273e7019238d5239
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-12 19:33:58 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - CUSTOM EXEPTION/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using EXERCISE_01___CUSTOM_EXEPTION.Entities;
+namespace EXERCISE_01___CUSTOM_EXEPTION {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            
+            if (checkOut <= checkIn) {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in");                
+            }
+            else {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " +reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if(checkIn < now || checkOut < now) {
+                    Console.WriteLine("Error in reservation: must be future dates! ");
+                }
+                else if(checkOut <= checkIn) {
+                    Console.WriteLine("Error ind reservation: must be future dates! ");
+                }
+                else { 
+                    reservation.UpdateDates(checkIn, checkOut)
+                }
+            }
+
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
+
